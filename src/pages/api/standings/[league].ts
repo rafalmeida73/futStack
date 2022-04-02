ac6a1df04aace720132cf523a3f2123e72621566
Rafael Santana <rafael.santana@webjump.com.br>
2022-04-02 16:55:34 -0300

    fix: year number

--- src/pages/api/standings/[league].ts
+++ src/pages/api/standings/[league].ts
@@ -3,8 +3,8 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { Stadings } from '../../../util/types/stadings';
 
 const standings = async (req: NextApiRequest, res: NextApiResponse) => {
-  const year = new Date().getFullYear();
   const { league } = req.query;
+  const year = league === '71' ? new Date().getFullYear() : '2021';
 
   if (req.method === 'GET') {
     const { data } = await axios.get<Stadings>(`https://v3.football.api-sports.io/standings?league=${league}&season=${year}`, {
